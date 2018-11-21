32f3632bfb22161c1d2c20815236c13ea87499c6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-20 23:11:26 -0500

    1.0.15: repeat of earlier mistake, but revealed that i should not have been doing it that way anyway

--- cli/lint.sh
+++ cli/lint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
+tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}lint/tslint.json
 
-tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}test/tslint.json
+tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}lint/tslint-test.json
