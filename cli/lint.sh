db8d0079b4ffe709d89c05bbae5b2cb996e135cb
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-20 22:28:36 -0500

    1.0.13: lint test as well

--- cli/lint.sh
+++ cli/lint.sh
@@ -4,4 +4,4 @@ set -e
 
 tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
 
-tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
+tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}/test/tslint.json
