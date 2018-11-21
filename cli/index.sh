8c74a73628b20c89c7ccaa7ea78fd2bb8b11400f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-20 19:04:19 -0500

    support the case where the parent repo also has tslint

--- cli/index.sh
+++ cli/index.sh
@@ -4,6 +4,7 @@ set -e
 
 CMD="$1"
 export UTILITIES_DIRECTORY="./node_modules/@musical-patterns/utilities/"
+export PATH=${PATH}:./node_modules/.bin/:${UTILITIES_DIRECTORY}/node_modules/.bin/
 
 if [[ ${CMD} == "lint" ]] ; then
     sh ${UTILITIES_DIRECTORY}/cli/lint.sh
--- cli/lint.sh
+++ cli/lint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-${UTILITIES_DIRECTORY}node_modules/.bin/tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
+tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
 
-${UTILITIES_DIRECTORY}/node_modules/.bin/tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
+tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}tslint.json
