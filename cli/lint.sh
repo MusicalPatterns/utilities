8e5bcd92cf2d46b4e1fdf8117d82c0167a691b6f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-24 10:37:39 -0800

    1.0.24: that wasnt quite right

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
-mv ${UTILITIES_DIRECTORY}tslint.json ${UTILITIES_DIRECTORY}tslint-self.json
+cp ${UTILITIES_DIRECTORY}tslint.json ./tslint-self.json
+
+mv ./tslint.json ./tslint-user.json
 
 cp ${UTILITIES_DIRECTORY}lint/tslint-main.json .
 mv tslint-main.json tslint.json
@@ -10,7 +12,9 @@ mv tslint.json tslint-main.json
 cp ${UTILITIES_DIRECTORY}lint/tslint-test.json .
 mv tslint-test.json tslint.json
 tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json
+
 rm tslint.json
 rm tslint-main.json
+rm tslint-self.json
 
-mv ${UTILITIES_DIRECTORY}tslint-self.json ${UTILITIES_DIRECTORY}tslint.json
+mv ./tslint-user.json ./tslint.json
