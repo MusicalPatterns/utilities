1c5f3fcd204b3a27eadd303c0b3998679b5adc18
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-23 18:03:30 -0800

    1.0.20: achieve renewment of support for subdirectory tslint configurations by copying tslint up into user directory and then deleting it afterward

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
-set -e
-
+cp ${UTILITIES_DIRECTORY}lint/tslint.json .
 tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json
+mv tslint.json tslint-main.json
 
-tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}lint/tslint-test.json
+cp ${UTILITIES_DIRECTORY}lint/tslint-test.json .
+mv tslint-test.json tslint.json
+tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json
+rm tslint.json
+rm tslint-main.json
