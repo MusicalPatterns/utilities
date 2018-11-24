482561ca49ca09855c5e886e23aec75a3c19eee0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-23 17:46:17 -0800

    1.0.19: experiment with avoiding passing config file path to tslint, hoping that this is what causes it not to find tslint config files throughout directories it lints

--- cli/lint.sh
+++ cli/lint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json -c ${UTILITIES_DIRECTORY}lint/tslint.json
+tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json
 
 tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json -c ${UTILITIES_DIRECTORY}lint/tslint-test.json
