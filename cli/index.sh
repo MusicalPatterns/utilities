4ed0a335b90dbc401aabc1d5f923ab1dc879f73f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-26 14:16:39 -0800

    1.0.0 as shared: rename from utilities to shared

--- cli/index.sh
+++ cli/index.sh
@@ -4,7 +4,7 @@ OPT="$2"
 if [[ ${OPT} == "self" ]] ; then
     export UTILITIES_DIRECTORY="./"
 else
-    export UTILITIES_DIRECTORY="./node_modules/@musical-patterns/utilities/"
+    export UTILITIES_DIRECTORY="./node_modules/@musical-patterns/shared/"
 fi
 
 export PATH=${PATH}:./node_modules/.bin/:${UTILITIES_DIRECTORY}node_modules/.bin/
