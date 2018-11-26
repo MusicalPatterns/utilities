e42c08e356da8f269980ea87bccd6dba6203c5e3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-26 15:56:16 -0800

    1.0.4: prevent publishing when the build fails

--- cli/build.sh
+++ cli/build.sh
@@ -13,9 +13,14 @@ mv tsconfig-common.json tsconfig-common-user.json > /dev/null 2>&1 || true
 mv tsconfig-common-tmp.json tsconfig-common.json
 
 eval "${CMD}"
+export BUILD_FAILED=$?
 
 rm tsconfig.json
 mv tsconfig-user.json tsconfig.json > /dev/null 2>&1 || true
 
 rm tsconfig-common.json
 mv tsconfig-common-user.json tsconfig-common.json > /dev/null 2>&1 || true
+
+if [[ ${BUILD_FAILED} != 0 ]] ; then
+	exit 1
+fi
