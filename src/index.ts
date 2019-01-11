5f363c510c777db9ac11b519cce561a932357541
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-10 20:00:38 -0800

    1.0.147: ms

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,9 @@ export {
     testIsCloseTo,
     testArraysAreClose,
 } from './testing'
+export {
+    MILLISECONDS_PER_SECOND,
+} from './time'
 
 export {
     Maybe,
---
+++ src/time/constants.ts
@@ -0,0 +1,7 @@
+// tslint:disable:no-magic-numbers
+
+const MILLISECONDS_PER_SECOND: number = 1000
+
+export {
+    MILLISECONDS_PER_SECOND,
+}
