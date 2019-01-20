62d18855a6b9c3e473fceca1b9f52344ea56574b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 10:53:25 -0800

    1.0.154: more helpful failure for test arrays are close

--- src/testing/testArraysAreClose.ts
+++ src/testing/testArraysAreClose.ts
@@ -4,7 +4,9 @@ const testArraysAreClose: <T>(actual: T[], expected: T[]) => void =
     <T>(actual: T[], expected: T[]): void => {
         expected.forEach((expectedElement: T, index: number): void => {
             expect(testIsCloseTo(actual[ index ], expectedElement))
-                .toBeTruthy()
+                .toBeTruthy(
+                    `Elements not close at index ${index}: actual ${actual[ index ]} vs expected ${expectedElement}`,
+                )
         })
     }
 
