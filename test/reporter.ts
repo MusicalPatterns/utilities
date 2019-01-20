61e17d6727233f651540eacfd6efd579c5142a7b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-20 12:04:16 -0800

    1.0.158: ignore updates

--- test/reporter.ts
+++
@@ -1,7 +0,0 @@
-// tslint:disable:no-implicit-dependencies
-import { SpecReporter } from 'jasmine-spec-reporter'
-
-jasmine.getEnv()
-    .clearReporters()
-jasmine.getEnv()
-    .addReporter(new SpecReporter())
