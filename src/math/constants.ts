f431cf2c3b88ac527f6e95bc9d1f6e0f418f4232
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 08:25:45 -0800

    1.0.209: more number constants

--- src/math/constants.ts
+++ src/math/constants.ts
@@ -1,4 +1,4 @@
-// tslint:disable no-magic-numbers
+// tslint:disable:no-magic-numbers max-file-line-count
 
 import { Count, Index, Offset, Power, Scalar, to } from '../nominal'
 
@@ -26,7 +26,9 @@ const TWO_DIMENSIONAL: Count = to.Count(2)
 const THREE_DIMENSIONAL: Count = to.Count(3)
 
 const SQUARED: Power = to.Power(2)
+const CUBED: Power = to.Power(3)
 const SQUARE_ROOT: Power = to.Power(1 / 2)
+const CUBE_ROOT: Power = to.Power(1 / 3)
 
 const EVEN: number = 2
 const TWO: number = 2
@@ -35,6 +37,22 @@ const NEGATIVE: Scalar = to.Scalar(-1)
 
 const ONCE: Count = to.Count(1)
 const TWICE: Count = to.Count(2)
+const THRICE: Count = to.Count(3)
+const FRICE: Count = to.Count(4)
+const FICE: Count = to.Count(5)
+const ZEROTH: Index = to.Index(0)
+const FIRST: Index = to.Index(1)
+const SECOND: Index = to.Index(2)
+const THIRD: Index = to.Index(3)
+const FOURTH: Index = to.Index(4)
+const FIFTH: Index = to.Index(5)
+const SIXTH: Index = to.Index(6)
+const SEVENTH: Index = to.Index(7)
+const EIGHTH: Index = to.Index(8)
+const NINTH: Index = to.Index(9)
+const TENTH: Index = to.Index(10)
+const ELEVENTH: Index = to.Index(11)
+const TWELFTH: Index = to.Index(12)
 
 const EVERY_OTHER: Scalar = to.Scalar(2)
 
@@ -73,4 +91,22 @@ export {
     MULTIPLICATIVE_IDENTITY,
     ARBITRARILY_LARGE_NUMBER,
     NEGATIVE,
+    CUBE_ROOT,
+    CUBED,
+    THRICE,
+    FRICE,
+    FICE,
+    ZEROTH,
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    SIXTH,
+    SEVENTH,
+    EIGHTH,
+    NINTH,
+    TENTH,
+    ELEVENTH,
+    TWELFTH,
 }
