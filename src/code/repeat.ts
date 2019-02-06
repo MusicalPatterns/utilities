9afefb7a27333eede1451e8f6b880fc396a33be3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 16:54:16 -0800

    ok for Cardinals to be called count

--- src/code/repeat.ts
+++ src/code/repeat.ts
@@ -1,9 +1,9 @@
 import { Cardinal, from } from '../nominal'
 
-const repeat: <T>(array: T[], cardinal: Cardinal) => T[] =
-    <T>(array: T[], cardinal: Cardinal): T[] => {
+const repeat: <T>(array: T[], count: Cardinal) => T[] =
+    <T>(array: T[], count: Cardinal): T[] => {
         let repeatedArray: T[] = []
-        for (let i: number = 0; i < from.Cardinal(cardinal); i += 1) {
+        for (let i: number = 0; i < from.Cardinal(count); i += 1) {
             repeatedArray = repeatedArray.concat(array)
         }
 
