abaf5f42e81982ca9ec42fb498750dea22fa31da
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-07 20:44:57 -0800

    1.0.230: correct Number typing of operations

--- src/math/evenOrOdd.ts
+++ src/math/evenOrOdd.ts
@@ -1,12 +1,14 @@
+// tslint:disable ban-types
+
 import { TWO } from './constants'
 import { dividesEvenly } from './dividesEvenly'
 
-const isEven: <T extends number>(n: T) => boolean =
-    <T extends number>(n: T): boolean =>
+const isEven: <T extends Number>(n: T) => boolean =
+    <T extends Number>(n: T): boolean =>
         dividesEvenly(n, TWO)
 
-const isOdd: <T extends number>(n: T) => boolean =
-    <T extends number>(n: T): boolean =>
+const isOdd: <T extends Number>(n: T) => boolean =
+    <T extends Number>(n: T): boolean =>
         !isEven(n)
 
 export {
