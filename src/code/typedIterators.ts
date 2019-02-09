9ea69c9ed230d9484168cbb610d92073aeb2909c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-08 16:30:43 -0800

    get typed callbacks right

--- src/code/typedIterators.ts
+++ src/code/typedIterators.ts
@@ -1,23 +1,22 @@
 import { from, Ordinal } from '../nominal'
-import { ForEachCallback, MapCallback, ReduceCallback } from './types'
 
 const slice: <T>(array: T[], initial: Ordinal, terminal: Ordinal) => T[] =
     <T>(array: T[], initial: Ordinal, terminal: Ordinal): T[] =>
         array.slice(from.Ordinal(initial), from.Ordinal(terminal))
 
-const forEach: <T>(array: T[], fn: ForEachCallback<T>) => void =
-    <T>(array: T[], fn: ForEachCallback<T>): void => {
+const forEach: <T>(array: T[], fn: (el: T, index: Ordinal) => void) => void =
+    <T>(array: T[], fn: (el: T, index: Ordinal) => void): void => {
         // @ts-ignore
         array.forEach(fn)
     }
 
-const map: <T, U>(array: T[], fn: MapCallback<T, U>) => U[] =
-    <T, U>(array: T[], fn: MapCallback<T, U>): U[] =>
+const map: <T, U>(array: T[], fn: (el: T, index: Ordinal) => U) => U[] =
+    <T, U>(array: T[], fn: (el: T, index: Ordinal) => U): U[] =>
         // @ts-ignore
         array.map(fn)
 
-const reduce: <T, U>(array: T[], fn: ReduceCallback<T, U>, accumulator: U) => U =
-    <T, U>(array: T[], fn: ReduceCallback<T, U>, accumulator: U): U =>
+const reduce: <T, U>(array: T[], fn: (accumulator: U, el: T, index: Ordinal) => U, accumulator: U) => U =
+    <T, U>(array: T[], fn: (accumulator: U, el: T, index: Ordinal) => U, accumulator: U): U =>
         // @ts-ignore
         array.reduce(fn, accumulator)
 
