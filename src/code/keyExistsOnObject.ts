16d028a73f73faad48c09753bfa461a8f2bd3d7e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-07 08:50:44 -0800

    1.0.317: was overthinking key exists on object

--- src/code/keyExistsOnObject.ts
+++ src/code/keyExistsOnObject.ts
@@ -1,7 +1,5 @@
-import { DictionaryOf } from './types'
-
-const keyExistsOnObject: <T extends DictionaryOf<unknown>>(key: string, object: T) => boolean =
-    <T extends DictionaryOf<unknown>>(key: string, object: T): boolean =>
+const keyExistsOnObject: <T>(key: string, object: T) => boolean =
+    <T>(key: string, object: T): boolean =>
         key in object
 
 export {
