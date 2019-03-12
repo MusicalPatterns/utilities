3df7235c26a48496063f00b33b3bdf3acc2cd281
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-12 13:31:52 -0700

    1.0.331: lets try out redux-ignore

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from 'redux'
+import { filterActions } from 'redux-ignore'
 import { TypedMap } from '../code'
 import { ActionForState } from './types'
 
@@ -7,13 +8,19 @@ const computeReducer:
     <State>({ initialState }: { initialState: TypedMap<State> }): Reducer<TypedMap<State>, ActionForState<State>> => {
         const stateKeys: string[] = Object.keys(initialState.toJS())
 
-        return (state: TypedMap<State> = initialState, action: ActionForState<State>): TypedMap<State> => {
-            if (!stateKeys.includes(action.type as string)) {
-                return state
+        const reducer: Reducer<TypedMap<State>, ActionForState<State>> =
+            (state: TypedMap<State> = initialState, action: ActionForState<State>): TypedMap<State> => {
+                if (!stateKeys.includes(action.type as string)) {
+                    return state
+                }
+
+                return state.set(action.type as keyof State, action.data as State[keyof State])
             }
 
-            return state.set(action.type as keyof State, action.data as State[keyof State])
-        }
+        return filterActions(
+            reducer as Reducer<TypedMap<State>>,
+            stateKeys,
+        ) as Reducer<TypedMap<State>, ActionForState<State>>
     }
 
 export {
