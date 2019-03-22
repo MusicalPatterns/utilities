dbcf8d9ac9a0b558c78b55f17763582c355f04fc
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-21 20:29:47 -0700

    1.0.381: better interface for test goes quadratically

--- src/testing/testQuadraticArrays.ts
+++ src/testing/testQuadraticArrays.ts
@@ -1,23 +1,27 @@
 // tslint:disable ban-types
 
-import { isUndefined } from '../code'
-import { computeDeltas, computeIntervals } from '../math'
-import { Scalar } from '../nominal'
+import { isUndefined, Maybe } from '../code'
+import { computeDeltas, computeIntervals, quotient } from '../math'
+import { Scalar, to } from '../nominal'
 import { testAllValuesAreTheSame } from './testAllValuesAreTheSame'
 import { testGoesMonotonically, testGoesMonotonicallyByAFactorOf } from './testMonotonicArrays'
 
 const testGoesQuadratically: <NumericElementType extends Number = Number>(
     array: NumericElementType[],
-    expectedFactor?: Scalar,
     expectedBeginValue?: NumericElementType,
+    expectedEndValue?: NumericElementType,
     precision?: number,
 ) => void =
     <NumericElementType extends Number = Number>(
         array: NumericElementType[],
-        expectedFactor?: Scalar,
         expectedBeginValue?: NumericElementType,
+        expectedEndValue?: NumericElementType,
         precision?: number,
     ): void => {
+        let expectedFactor: Maybe<Scalar> = undefined
+        if (!isUndefined(expectedBeginValue) && !isUndefined(expectedEndValue)) {
+            expectedFactor = to.Scalar(quotient(expectedEndValue, expectedBeginValue))
+        }
         if (isUndefined(expectedFactor)) {
             testGoesMonotonically(array, undefined, expectedBeginValue)
         }
