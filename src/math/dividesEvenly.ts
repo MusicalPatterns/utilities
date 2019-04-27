9c7983a1cd122e820668931a46c3b5f3855c6447
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-27 16:34:58 -0700

    1.0.537: improve divides evenly

--- src/math/dividesEvenly.ts
+++ src/math/dividesEvenly.ts
@@ -1,8 +1,14 @@
 // tslint:disable no-any
 
-const dividesEvenly: <T1, T2>(value: T1, modulus: T2) => boolean =
-    <T1, T2>(value: T1, modulus: T2): boolean =>
-        value as unknown as number % (modulus as unknown as number) === 0
+const dividesEvenly: <NumericValue extends Number, AnotherNumericValue extends Number>(
+    numeral: NumericValue,
+    modulus: AnotherNumericValue,
+) => boolean =
+    <NumericValue extends Number, AnotherNumericValue extends Number>(
+        numeral: NumericValue,
+        modulus: AnotherNumericValue,
+    ): boolean =>
+        numeral as unknown as number % (modulus as unknown as number) === 0
 
 export {
     dividesEvenly,
